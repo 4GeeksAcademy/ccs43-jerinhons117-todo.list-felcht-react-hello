8f7acb97493a1becf43d2c75e50a2f25ccdccac4
jerinhons117 <131413483+jerinhons117@users.noreply.github.com>
2023-07-14 23:48:42 +0000

    finalizado el todolist +  fetch

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 
 const TodoList = () => {
+    
+    const API_URL = "https://assets.breatheco.de/apis/fake/todos/user/jerinhons117";
     const [task, setTask] = useState("");
     const [taskList, setTaskList] = useState([]);
     const [error, setError] = useState(false)
@@ -17,11 +19,148 @@ const TodoList = () => {
                 alert("Please add a task!");
                 return;
             }
-            setTaskList([...taskList, task]);
+            putToDos(task);
             setTask("");
             setError(false);
         }
     }
+
+    /*const resetlist = () => {
+        setTaskList("");
+        console.log("reseteado");
+    }*/
+
+    async function getToDos(){
+        try{
+            const response = await fetch(API_URL);
+            console.log(response)
+            if (response.status == 404){
+                postToDos()
+                return;
+            }
+
+            if (response.status != 200){
+                console.log("hay un error en el Get")
+                return;
+            }
+            const body = await response.json();
+            console.log(body);
+            setTaskList(body);
+
+       
+        } catch(error){
+            console.log(error)
+        }
+    }
+     async function trashToDo(clearTask){
+            try{
+                const response = await fetch(API_URL, {
+                    method: "PUT",
+                    headers:{
+                        "Content-Type":"application/json"
+                    },
+                    body:JSON.stringify(clearTask) 
+                });
+                console.log(response)
+                if (response.status != 200){
+                    console.log("hay un error en el PUT")
+                    return;
+                }
+                const body = await response.json();
+                console.log(body);
+                getToDos()
+                
+            }
+            catch(error){
+                console.log(error)
+            }
+        };
+
+    async function putToDos(newTask){
+        const taskObject ={
+            label: newTask,
+            done: false
+        }
+        try{
+            const response = await fetch(API_URL, {
+                method: "PUT",
+                headers: {
+					"Content-Type": "application/json"
+				},
+                body: JSON.stringify([...taskList, taskObject])
+          
+            });
+            console.log(response);
+            if (response.status != 200){
+                console.log("hay un error en el Get")
+                return;
+            }
+            const body = await response.json();
+            console.log(body);
+            getToDos();
+
+
+            
+        } 
+        catch(error){
+            console.log("error")
+        }
+    }
+
+    async function postToDos(postUser){
+        try{
+            const response = await fetch(API_URL,{
+                method: "POST",
+                headers:{
+                    "content-type":"application/json"
+                },
+
+                body:JSON.stringify([])
+            })
+            console.log(response);
+            if(response.status !=200){
+                console.log("hay un error en el POST")
+                return;
+            }
+            getToDos()
+            
+        } 
+        
+        catch(error){
+            console.log(error);
+        }
+
+    }
+
+
+    async function deleteToDos(deleteTask){
+        try{
+            const response = await fetch(API_URL,{
+                method:"DELETE",
+                headers:{
+                    "content-type":"application/json"
+                },
+            });
+            console.log(response)
+            if (response.status !=200){
+                console.log("hay un error en el DELETE")
+                return;
+            }
+            postToDos();
+
+        } catch(error){
+            console.log(error)
+
+        }
+    }
+
+    useEffect(() => {
+        console.log("inicia la aplicacion") 
+        getToDos()
+        
+    },[])
+
+    
     return (
 
         <div className="body">
@@ -30,9 +169,7 @@ const TodoList = () => {
                     <h1 className={`${error ? "text-danger" : "light"}`}>ToDo List</h1>
                 </div>
                 <div className="card_list">
-                <label htmlFor="task" >
-
-                </label>
+                
                 <input className={`${error ? "bg-danger" : "light"}`} value={task} onKeyDown={addTask} onChange={handleTask} placeholder="create a task"/>
                     <div>
                         <div>
@@ -41,14 +178,39 @@ const TodoList = () => {
                                 <p> there is no task to add</p>
                             ):(
                                 taskList.map((task, index) => {
-                                    return <p key={index} className="list"> {task} {""} <i className="fa-solid fa-x equis" onClick={() => setTaskList(taskList.filter((t, currentIndex ) => index != currentIndex))}></i></p>
+                                    return (
+                                        <p 
+                                            key={index} 
+                                            className="list"
+                                        > 
+                                            {task.label} {""} 
+                                            <i 
+                                                className="fa-solid fa-x equis" 
+                                                onClick={() =>{
+                                                    const newToDo = taskList.filter((t, currentIndex ) => index != currentIndex)
+                                                    if (newToDo.length == 0){
+                                                        deleteToDos()
+                                                        return;
+                                                        
+                                                    }
+                                                    
+                                                    trashToDo(newToDo);
+                                                }}
+                                            ></i>
+                                        </p>)
                                 })
+                                
                             )
+                            
                         }
                         </div>
                         <div className="number_list">
                             <p>{taskList.length} task</p>
                         </div>
+                    <button className="boton" onClick={deleteToDos}
+                    >
+                        refresh
+                    </button>
 
                     </div>
                 
